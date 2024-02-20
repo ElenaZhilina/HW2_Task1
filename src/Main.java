27ed1fb57c0685fdaeb66190809a5adb06b99e5c
Elena <elenazhilina@yandex.ru>
2024-02-20 14:32:05 +0600

    Java Code added

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,8 @@
 public class Main {
+    public static void main(String[] args) {
+        int ticket = 13_676;
+        int mileCost = 20;
+
+        System.out.println("количество бонусных миль = " + ticket / mileCost);
+    }
 }
